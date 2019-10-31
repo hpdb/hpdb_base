33d93c2bf08612692015427e7d60b45a6d9ace71
hieplpvip <baohiep2013@gmail.com>
2019-10-31 22:32:52 +0700

    Update

--- INSTALL.sh
+++ INSTALL.sh
@@ -35,7 +35,7 @@ ln -fs $anacondabin/python $rootdir/bin
 ln -fs $anacondabin/pip $rootdir/bin
 ln -fs $anacondabin/conda $rootdir/bin
 $anacondabin/conda install -y biopython
-$anacondabin/pip install regex mysqlclient
+$anacondabin/pip install regex
 echo "
 ------------------------------------------------------------------------------
                          Anaconda2 Installed
