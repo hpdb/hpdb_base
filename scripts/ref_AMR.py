d913257b6e13421940a0e1ceeee6d12465a20f77
hieplpvip <baohiep2013@gmail.com>
2019-12-03 17:46:04 +0700

    Update

--- scripts/ref_AMR.py
+++ scripts/ref_AMR.py
@@ -1,11 +1,11 @@
 AMR = []
 AMR.append({'name': '23S rRNA (1)', 'start': 445248, 'end': 448222, 'type': 'nu', \
             'subs': [{'pos': 2145, 'orig': 'A'}, \
-                     {'pos': 2146, 'orig': 'C'}]})
+                     {'pos': 2146, 'orig': 'A'}]})
 
 AMR.append({'name': '23S rRNA (2)', 'start': 1473917, 'end': 1476892, 'type': 'nu', \
             'subs': [{'pos': 2145, 'orig': 'A'}, \
-                     {'pos': 2146, 'orig': 'C'}]})
+                     {'pos': 2146, 'orig': 'A'}]})
 
 AMR.append({'name': 'gyrA', 'type': 'prot', \
             'subs': [{'pos': 86, 'orig': 'N'}, \
