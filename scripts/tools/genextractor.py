6948971a86929b5fb64b2ea2e78719a3180c3507
hieplpvip <baohiep2013@gmail.com>
2020-01-17 18:03:28 +0700

    Update

--- scripts/tools/genextractor.py
+++ scripts/tools/genextractor.py
@@ -8,10 +8,16 @@ def run(configs):
   utils.runsnippy('ref.fasta', 'input.fasta')
   raw = utils.runblast('blastn', 'snippy/snps.consensus.fa', 'input.fasta', '0.0001', '10 sstart send sseq').splitlines()[0].split(',')
   
+  start = int(raw[0])
+  end = int(raw[1])
+  if start < end:
+    start, end = end, start
+  length = end - start + 1
+  
   with open('result.txt', 'w') as f:
-    f.write('Start: %s\n' % raw[0])
-    f.write('End: %s\n' % raw[1])
-    f.write('Length: %s\n' % str(int(raw[1]) - int(raw[0])))
+    f.write('Start: %s\n' % str(start))
+    f.write('End: %s\n' % str(end))
+    f.write('Length: %s\n' % str(length))
     f.write('\n\n')
     f.write(textwrap.fill(raw[2], 60))
   
