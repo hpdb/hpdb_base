40712b92505b303703046526115e399d2eaa87e9
hieplpvip <baohiep2013@gmail.com>
2020-02-12 16:54:00 +0700

    Update

--- make-run.sh
+++ make-run.sh
@@ -2,8 +2,9 @@
 # make-run.sh
 # make sure core is always running.
 
+echo 'a' > /home/hpdb/text.x
 source "/home/hpdb/.bashrc"
-echo $HPDB_BASE
+echo $HPDB_BASE > /home/hpdb/text.x
 if ps ax | grep -v grep | grep runcore > /dev/null
 then
   exit
